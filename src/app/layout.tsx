bb5cb5c8f3c545a340b8e402d0d4214449151121
mlekhi <maya.lekhi1@gmail.com>
2024-09-14 20:04:05 -0400

    tiny commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider> <Navbar />{children}</ConvexClientProvider>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
     </html>
   );
